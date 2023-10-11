43cc6d070ca485134720dd1bcf261d989a582742
Jauhari-01 <shubham.jauhari009@gmail.com>
2023-10-11 19:16:50 +0530

    Completed portfolio section

--- shubhamjauhari/src/constant/index.js
+++ shubhamjauhari/src/constant/index.js
@@ -1,3 +1,7 @@
+import P1 from '../assets/images/ProjectPic1.png';
+import B1 from '../assets/images/BackEndProject1.png';
+import P2 from '../assets/images/ProjectPic11.png';
+
 export const aboutMeContent=[
     {
         content: `"Hello, I'm Shubham Jauhari, a dedicated software developer with a strong foundation in both front-end and back-end technologies. In addition to my software development skills, I'm currently gaining valuable experience as a Data Structures and Algorithms (DSA) mentor intern at AccioJob.`
@@ -58,4 +62,39 @@ export const backEndTechStack = [
         name: 'FIREBASE',
         experience: 'Basic'
     }
+]
+
+export const projects=[
+    {
+        type: 'Frontend Project',
+        name: 'STube',
+        picLink: P1,
+        about: ' a web application developed with React.js and integrated with Rapid APIs to recreate the popular video-sharing platform, YouTube. This project aims to replicate the core features and user interface of YouTube, allowing users to browse, search for, and view videos. ',
+        liveLink: 'https://stube.vercel.app/',
+        githubLink: 'https://github.com/Jauhari-01/stube'
+    },
+    {
+        type: 'Backend Project',
+        name: 'Dosify',
+        picLink: B1,
+        about: 'Set of APIs using Spring Boot, following RESTful principles. These APIs allowed users to perform various actions related vaccination booking.',
+        liveLink: '',
+        githubLink: 'https://github.com/Jauhari-01/Dosify_Covid-19-Vaccination-Booking-Application'
+    },
+    {
+        type: 'Backend Project',
+        name: 'FlipCommerce',
+        picLink: B1,
+        about: 'FlipCommerce is an e-commerce application with a wide range of APIs that facilitate various functionalities related to online shopping.',
+        liveLink: '',
+        githubLink: 'https://github.com/Jauhari-01/FlipCommerce'
+    },
+    {
+        type: 'Frontend Project',
+        name: 'PicFinder',
+        picLink: P2,
+        about: 'React Project for demonstration of WebAPIs',
+        liveLink: 'https://picfinder.vercel.app/',
+        githubLink: 'https://github.com/Jauhari-01/picfinder'
+    },
 ]
